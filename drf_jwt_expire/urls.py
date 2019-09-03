3add0538e2f4c91fde112c0076ded5343faa2a5f
sprumin <sprumin123@gmail.com>
2019-09-03 18:19:27 +0900

    add test application

--- drf_jwt_expire/urls.py
+++ drf_jwt_expire/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/token/', obtain_jwt_token),
     path('api/token/verify/', verify_jwt_token),
     path('api/token/refresh/', refresh_jwt_token),
+    path('api/app/', include('app.urls'))
 ]
